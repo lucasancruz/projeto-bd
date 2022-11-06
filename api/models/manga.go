d37ab5a38af6e5d07557ee46cb51d76d91eb30e6
Lucas Andrade Cruz <ddlukas0@gmail.com>
2022-11-06 18:54:25 -0300

    Criando Model do Mangá

--- api/models/manga.go
+++ api/models/manga.go
@@ -1,1 +1,11 @@
 package models
+
+type Manga struct {
+	Id              int    `json:"id"`
+	Titulo          string `json:"titulo"`
+	Sinopse         string `json:"sinopse"`
+	Imagem          string `json:"imagem"`
+	DataCriacao     string `json:"data_criacao"`
+	DataFinalizacao string `json:"data_finalizacao"`
+	EditoraId       int    `json:"editora_id"`
+}
