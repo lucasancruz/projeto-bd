922433f8459188ebe49944adf546f5a4331eb51a
Lucas Andrade Cruz <ddlukas0@gmail.com>
2022-11-07 21:53:23 -0300

    Adicionando conexão ao banco de dados

--- api/db/db.go
+++ api/db/db.go
@@ -1,1 +1,44 @@
 package db
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectToDatabase() *gorm.DB {
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	timezone := "America%2FFortaleza"
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=%s",
+		dbUser,
+		dbPass,
+		dbHost,
+		dbName,
+		timezone,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed connecting to database!")
+	}
+
+	return db
+}
+
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+
+	if err != nil {
+		panic("Failed to close connection to database!")
+	}
+
+	dbSQL.Close()
+}
--- api/main.go
+++ api/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"api/config"
+	"api/db"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func main() {
+	config.LoadEnv()
+
+	DB = db.ConnectToDatabase()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
