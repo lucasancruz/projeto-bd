6043bb763ef636c03b63e0906ca948505a493be2
Lucas Andrade Cruz <ddlukas0@gmail.com>
2022-11-19 19:19:24 -0300

    Atualizando rotas do Mangá

--- api/routes/manga.go
+++ api/routes/manga.go
@@ -10,7 +10,8 @@ func addMangaRoutes(rg *gin.RouterGroup) {
 	manga := rg.Group("/manga")
 
 	manga.POST("/", controllers.CreateManga)
+	manga.GET("/", controllers.FindAllMangas)
 	manga.PUT("/:id", controllers.UpdateManga)
 	manga.GET("/:id", controllers.FindMangaById)
-	manga.GET("/", controllers.FindAllManga)
+	manga.DELETE("/:id", controllers.DeleteManga)
 }
