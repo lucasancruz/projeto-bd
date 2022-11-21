7d5d62b15416f9cf7f4f8ca98f3f649609f4981a
Lucas Andrade Cruz <ddlukas0@gmail.com>
2022-11-20 21:26:16 -0300

    Adicionando constantes

---
+++ app/lib/shared/constants.dart
@@ -0,0 +1,47 @@
+import 'package:app/models/manga.dart';
+
+final List<Manga> mangas = [
+  Manga(
+    id: 2,
+    titulo: "Chainsaw-Man", 
+    sinopse: "A história origina uma das adaptações mais esperadas de 2022. Em Chainsaw Man, o protagonista Denji é um adolescente que mora com o seu demônio de estimação, Ponchita, o Demônio da Motosserra. Apesar disso, seu dever é trabalhar como um Caçador de Dêmonios para pagar uma dívida que possui.",
+    imagem: "https://chainsawmanscan.com/wp-content/uploads/2022/09/71JOiChQYgL-1365x2048-1.jpeg", 
+    editora: "Shounen Jump", 
+    generos: ["Shounen", "Comédia", "Drama"], 
+    autores: ["Tatsuki Fujimoto"], 
+    capitulos: []
+  ),
+  Manga(
+    id: 5,
+    titulo: "Boa Noite, Punpun", 
+    sinopse: "Oyasumi Punpun (おやすみプンプン lit. 'Boa noite Punpun') é uma série de mangá escrita e ilustrada por Inio Asano sobre Onodera Punpun, uma criança normal representada na forma de um pássaro. A história o segue como ele lida com sua família disfuncional e amigos, seu interesse amoroso, sua adolescência e sua mente hiperativa.",
+    imagem: "https://m.media-amazon.com/images/I/51EQ5jMPsHL.jpg",
+    editora: "Big Comic Spirits", 
+    generos: ["Drama", "Seinen"], 
+    autores: ["Inio Asano"], 
+    capitulos: []
+  ),
+  Manga(
+    id: 6,
+    titulo: "Berserk", 
+    sinopse: "Berserk (ベルセルク Beruseruku) é uma série de mangá escrita e ilustrada por Kentaro Miura. Situado em um mundo de fantasia sombria inspirado na Europa medieval, a história gira em torno de Guts, um solitário mercenário, e Griffith, o líder de um bando de mercenários chamado de \"Bando do Falcão\".",
+    imagem: "https://m.media-amazon.com/images/I/A1KdivdH+sL.jpg",
+    editora: "Young Animals", 
+    generos: ["Drama", "Seinen", "Sobrenatural", "Fantasia"], 
+    autores: ["Kentaro Miura"], 
+    capitulos: []
+  ),
+  Manga(
+    id: 7,
+    titulo: "Kimi wa Houkago Insomnia", 
+    sinopse: "Nakami é um estudante dedicado, porém, sempre parece estar irritado, o que faz seus colegas de classe não gostarem dele. Porém essa atitude é devido ao fato dele ter insônia, passando a maior parte da noite em claro, sem conseguir dormir nem um minuto sequer. Durante a preparação do festival escolar, ele foi enviado contra a sua vontade para pegar cartolina na sala do observatório de astronomia da escola, um lugar que tem fama de ser mal-assombrado. Lá ele encontra um lugar calma e isolado para dormir, mas enquanto ele verificava o local ele encontra sua colega de classe Magari Isaki, que estava dormindo lá. Após ambos descobrirem sobre o problema de insônia um do outro, eles decidem dividir aquele local como refúgio para poderem dormir. Por quanto tempo será que eles poderão se aproveitar desse conforto? E será que a sala do observatório realmente é mal-assombrada?",
+    imagem: "https://mangayabu.top/wp-content/uploads/2022/07/c8a32fa69379b8c43965.jpg",
+    editora: "Big Comic Spirits", 
+    generos: ["Romance", "Comédia", "Drama"], 
+    autores: ["Makoto Ojiro"], 
+    capitulos: []
+  ),
+];
+
+const double kDefaultPadding = 12;
+const double kDefaultMargin = 8;
