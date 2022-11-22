e164f8182e7bd56609754a1b0b444e2394b0c4c2
Lucas Andrade Cruz <ddlukas0@gmail.com>
2022-11-21 22:00:02 -0300

    Ajuste nas datas dos models

--- api/models/capitulo.go
+++ api/models/capitulo.go
@@ -5,9 +5,8 @@ import "time"
 type Capitulo struct {
 	ID             uint      `gorm:"primaryKey" json:"id"`
 	MangaID        uint      `gorm:"not null" json:"manga_id"`
-	Manga          Manga     `json:"manga"`
 	Titulo         string    `gorm:"type:varchar(45);not null" json:"titulo"`
-	Numero         uint32    `gorm:"not null" json:"numero"`
+	Numero         uint32    `gorm:"primaryKey;autoIncrement:false;" json:"numero"`
 	Url            string    `gorm:"type:varchar(120);not null" json:"url"`
-	DataPublicacao time.Time `gorm:"type:datetime;not null" json:"data_publicacao"`
+	DataPublicacao time.Time `gorm:"type:datetime;not null" json:"data_publicacao,omitempty"`
 }
--- api/models/manga.go
+++ api/models/manga.go
@@ -1,16 +1,18 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type Manga struct {
 	ID              uint       `gorm:"primaryKey" json:"id"`
 	Titulo          string     `gorm:"type:varchar(120);not null" json:"titulo"`
 	Sinopse         string     `gorm:"type:text;not null" json:"sinopse"`
 	Imagem          string     `gorm:"type:varchar(150);not null" json:"imagem"`
-	DataCriacao     time.Time  `gorm:"type:date" json:"data_criacao"`
-	DataFinalizacao time.Time  `gorm:"type:date" json:"data_finalizacao"`
+	DataCriacao     *time.Time `gorm:"type:date" json:"data_criacao,omitempty"`
+	DataFinalizacao *time.Time `gorm:"type:date" json:"data_finalizacao,omitempty"`
 	EditoraID       uint       `gorm:"not null" json:"editora_id"`
-	Editora         Editora    `json:"editora"`
+	Editora         *Editora   `json:"editora,omitempty"`
 	Generos         []Genero   `gorm:"many2many:manga_generos;" json:"generos"`
 	Autores         []Autor    `gorm:"many2many:manga_autores;" json:"autores"`
 	Capitulos       []Capitulo `json:"capitulos"`
